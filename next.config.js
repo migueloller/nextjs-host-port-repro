65479fff4c7eb05c991b5ede5244d03c1e277302
Miguel Oller <maoa22@gmail.com>
2023-04-10 18:41:46 -0400

    Add missing host port reproduction

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path(.*)',
+          has: [{ type: 'host', value: '(?<host>.+)' }],
+          destination: '/:path?host=:host',
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig
